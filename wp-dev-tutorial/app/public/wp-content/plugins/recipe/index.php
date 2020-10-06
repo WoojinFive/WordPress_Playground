795cd46390a90a44b4100bb1e31d9c16b357bea0
woojinfive <kh3996@gmail.com>
2020-10-05 22:17:14 -0300

    Plugin Activation & Security

--- wp-dev-tutorial/app/public/wp-content/plugins/recipe/index.php
+++ wp-dev-tutorial/app/public/wp-content/plugins/recipe/index.php
@@ -7,3 +7,19 @@
  * Author URI: https://www.halifaxweb.dev
  * text domain: recipe
  */
+
+//  for the security. 워드프레스 실행 없이 직접 파일에 접근할 경우 대비
+ if( !function_exists( 'add_action' ) ) {
+   echo "Hi there! I'm just a plugin, not much I can do when called directly.";
+   exit;
+ }
+
+// Setup
+
+// Includes
+include( 'includes/activate.php' );
+
+// Hooks
+register_activation_hook( __FILE__, 'r_activate_plugin' ); // This function will be called when our plugin is activated.
+
+// Shortcodes
