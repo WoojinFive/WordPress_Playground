5eea609bba082d71fafa83f2dd8e57864a4c9d76
woojinfive <kh3996@gmail.com>
2020-10-05 21:21:26 -0300

    Creating our FIrst Plugin

---
+++ wp-dev-tutorial/app/public/wp-content/plugins/recipe/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Recipe
+ * Description: A simple WordPress plugin that allows user to create recipes and rate those recipes
+ * Version: 1.0
+ * Author: Woojin Oh
+ * Author URI: https://www.halifaxweb.dev
+ * text domain: recipe
+ */
