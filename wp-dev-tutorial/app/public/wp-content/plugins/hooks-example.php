2830bdedfe1e969b49426da69dce2840399b7508
woojinfive <kh3996@gmail.com>
2020-10-05 22:08:34 -0300

    Custom Hooks and Prioritization

--- wp-dev-tutorial/app/public/wp-content/plugins/hooks-example.php
+++ wp-dev-tutorial/app/public/wp-content/plugins/hooks-example.php
@@ -3,13 +3,28 @@
  * Plugin Name: Hooks Example
  */
  
-function ju_title( $title ) {
-  return 'Hooked ' . $title;
+// function ju_title( $title ) {
+//   return 'Hooked ' . $title;
+// }
+
+// function ju_footer_shoutout() {
+//   echo 'Hooked Example plugin was here. <br>';
+// }
+
+// function ju_footer_shoutout_v2() {
+//   echo 'Hooked Example plugin was here. Version 2. <br>';
+// }
+
+// add_filter( 'the_title', 'ju_title' );
+// add_action( 'wp_footer', 'ju_footer_shoutout' );
+// add_action( 'wp_footer', 'ju_footer_shoutout_v2', 5 ); // default : 10
+
+function ju_footer() {
+  do_action ( 'ju_custom_footer' );
 }
 
-function ju_footer_shoutout() {
-  echo 'Hooked Example plugin was here.';
+function ju_kill_wp() {
+  echo 'Hello!';
 }
-
-add_filter( 'the_title', 'ju_title' );
-add_action( 'wp_footer', 'ju_footer_shoutout' );
+add_action( 'wp_footer', 'ju_footer' );
+add_action ( 'ju_custom_footer', 'ju_kill_wp' );
