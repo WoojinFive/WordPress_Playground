989035ad047d919090568bb20e8cda13e101b3d7
woojinfive <kh3996@gmail.com>
2020-10-08 15:24:07 -0300

    Block Settings

--- wp-dev-tutorial/app/public/wp-content/plugins/recipe/blocks/app/recipe-block/index.js
+++ wp-dev-tutorial/app/public/wp-content/plugins/recipe/blocks/app/recipe-block/index.js
@@ -1,5 +1,27 @@
 // console.log( wp );
+import block_icons from '../icons/index';
+
+const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
 
 wp.blocks.registerBlockType( 'udemy/recipe', {
-  
+  title:            __( 'Recipe', 'recipe' ), // title: wp.i18n.__( 'Recipe', 'recipe' )
+  description:      __(
+    'Provides a short summary of a recipe.',
+    'recipe'
+  ),
+  // coomon, formatting, layouts, widgets, embed
+  category:         'common',
+  icon:             block_icons.wapuu,
+  keywords: [
+    __( 'Food', 'recipe' ),
+    __( 'Ingredients', 'recipe' ),
+    __( 'Meal Type', 'recipe' )
+  ],
+  supports: {
+    html:           false
+  },
+  save: () => {
+    return <p>Hello World!</p>
+  }
 } );
