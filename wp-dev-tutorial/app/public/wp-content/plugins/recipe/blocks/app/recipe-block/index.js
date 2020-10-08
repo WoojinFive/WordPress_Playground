dcd366ccde08026c4d9f1c49184e387eda62963c
woojinfive <kh3996@gmail.com>
2020-10-08 16:03:09 -0300

    Inspector Controls

--- wp-dev-tutorial/app/public/wp-content/plugins/recipe/blocks/app/recipe-block/index.js
+++ wp-dev-tutorial/app/public/wp-content/plugins/recipe/blocks/app/recipe-block/index.js
@@ -3,6 +3,8 @@ import block_icons from '../icons/index';
 
 const { registerBlockType } = wp.blocks;
 const { __ } = wp.i18n;
+const { InspectorControls } = wp.editor;
+const { PanelBody, PanelRow, TextControl, SelectControl } = wp.components;
 
 wp.blocks.registerBlockType( 'udemy/recipe', {
   title:            __( 'Recipe', 'recipe' ), // title: wp.i18n.__( 'Recipe', 'recipe' )
@@ -23,7 +25,70 @@ wp.blocks.registerBlockType( 'udemy/recipe', {
   },
   edit: ( props ) => {
     //console.log( props );
-    return (
+    return [
+      <InspectorControls>
+        <PanelBody title={ __( 'Basics', 'recipe' ) }>
+          <PanelRow>
+            <p>{ __( 'Configure the contents of your block here.', 'recipe' ) }</p>
+          </PanelRow>
+
+          <TextControl 
+            label={ __( 'Ingredients', 'recipe' ) } 
+            help={ __( 'Ex: tomatoes, lettuce, olive oil, etc.', 'recipe') }
+            value="test"
+            onChange={ (new_val) => {
+              console.log(new_val);
+            }}
+          />
+
+          <TextControl 
+            label={ __( 'Cooking Time', 'recipe' ) } 
+            help={ __( 'How long will this take to cook?', 'recipe') }
+            value="test"
+            onChange={ (new_val) => {
+              console.log(new_val);
+            }}
+          />
+
+          <TextControl 
+            label={ __( 'Utensils', 'recipe' ) } 
+            help={ __( 'Ex: spoon, knife, pots, pans', 'recipe') }
+            value="test"
+            onChange={ (new_val) => {
+              console.log(new_val);
+            }}
+          />
+
+          <SelectControl
+            label={ __( 'Cooking Experience', 'recipe' ) }
+            help={ __( 'How skilled should the reader be?', 'recipe' ) }
+            value="Beginner"
+            options={[
+              {value: 'Beginner', label: 'Beginner'},
+              {value: 'Intermediate', label: 'Intermediate'},
+              {value: 'Expert', label: 'Expert'}
+            ]}
+            onChange={ (new_val) => {
+              console.log(new_val);
+            }}
+          />
+
+          <SelectControl
+            label={ __( 'Meal Type', 'recipe' ) }
+            help={ __( 'When is this best eaten', 'recipe' ) }
+            value="Breakfast"
+            options={[
+              {value: 'Breakfast', label: 'Breakfast'},
+              {value: 'Lunch', label: 'Lunch'},
+              {value: 'Dinner', label: 'Dinner'}
+            ]}
+            onChange={ (new_val) => {
+              console.log(new_val);
+            }}
+          />
+
+        </PanelBody>
+      </InspectorControls>,
       <div className={props.className}>
         <ul className="list-unstyled">
           <li>
@@ -43,7 +108,7 @@ wp.blocks.registerBlockType( 'udemy/recipe', {
           </li>
         </ul>
       </div>
-    );
+    ];
   },
   save: ( props ) => {
     return <p>Hello World!</p>
