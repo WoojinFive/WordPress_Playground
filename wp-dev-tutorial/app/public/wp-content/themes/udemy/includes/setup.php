e050bd93cd6b1033b8e0599bab95ea4043680172
woojinfive <kh3996@gmail.com>
2020-10-13 21:41:13 -0300

    Getting started with Woocommerce Development

--- wp-dev-tutorial/app/public/wp-content/themes/udemy/includes/setup.php
+++ wp-dev-tutorial/app/public/wp-content/themes/udemy/includes/setup.php
@@ -6,6 +6,7 @@ function ju_setup_theme() {
   add_theme_support( 'custom-logo' );
   add_theme_support( 'automatic-feed-links' ); // for RSS readers to scan this document.
   add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+  add_theme_support( 'woocommerce' );
   /*
   add_theme_support( 'starter-content', [
     'widgets'                   =>  [
