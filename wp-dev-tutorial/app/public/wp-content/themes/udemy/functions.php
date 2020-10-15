f38300bbab1a3c3b1cb55085ec5a42ff79b62c76
woojinfive <kh3996@gmail.com>
2020-10-14 21:32:01 -0300

    Main Query Home Page Modifications

--- wp-dev-tutorial/app/public/wp-content/themes/udemy/functions.php
+++ wp-dev-tutorial/app/public/wp-content/themes/udemy/functions.php
@@ -17,6 +17,7 @@ include( get_theme_file_path( '/includes/utility.php' ) );
 include( get_theme_file_path( '/includes/buddypress/profile-posts.php' ) );
 include( get_theme_file_path( '/includes/admin/author-fields.php' ) );
 include( get_theme_file_path( '/includes/avatar.php' ) );
+include( get_theme_file_path( '/includes/home-query.php' ) );
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
@@ -36,5 +37,6 @@ add_action( 'edit_user_profile', 'ju_custom_user_profile_fields' );
 add_action( 'personal_options_update', 'ju_save_extra_profile_fields' );
 add_action( 'edit_user_profile_update', 'ju_save_extra_profile_fields' );
 add_action( 'avatar_defaults', 'ju_new_avatar');
+add_action( 'pre_get_posts', 'ju_modify_home_page_query' );
 
 // Shortcodes
